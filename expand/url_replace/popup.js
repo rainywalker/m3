ca8fc29d8374f5a5df601c4ed087873eb3b4cfc0
jaelomin_nhn <rainywalker@naver.com>
2014-02-12 17:37:27 +0900

    url replace commit

--- expand/url_replace/popup.js
+++ expand/url_replace/popup.js
@@ -30,6 +30,7 @@ function url_rep(){
 					var com_name = document.getElementById("com_name");
 					var local_ip = $(xml).find("addr").text();
 					ip.innerHTML = local_ip;
+					com_name.innerHTML = localStorage.key(localStg_key)
 					var doc_root = document.getElementById("doc_root");
 					var root_val = doc_root.value.toLowerCase();
 					//var key = root_val
@@ -41,8 +42,12 @@ function url_rep(){
 						//var ch_url = re_url.substr(2)
 						var sum = https + re_url
 						chrome.tabs.update({url: sum});
-						console.log(sum,re_url,localStorage.key(localStg_key))
+						console.log(sum,localStorage.key(localStg_key))
 					}
+					else {
+						console.log("your protocol = http")
+					}
+
 				}
 			})
 		}
@@ -58,38 +63,41 @@ function url_rep(){
 					var doc_root = document.getElementById("doc_root");
 					var btn_ipt = document.getElementById("btn_ipt");
 					btn_ipt.onclick = function(){
+						
 						var currentURL = tabArray[0].url.toLowerCase();
-						var first_URL = currentURL.substr(0,8); 
 						var url_addr = currentURL.substr(8);
 						var https = "http://";
 						var root_val = doc_root.value.toLowerCase();
-						var strArr = localStorage.getItem("strArr");
-						if (!strArr) {
-							//strArr = [];
-							localStorage.setItem(root_val,root_val);
-						}
-						else {
-							strArr = JSON.parse(strArr);
-						}
+						
 						var key = root_val
 						localStorage.setItem(key,root_val);
 						//strArr.push(key);
 						//localStorage.setItem(root_val,root_val);
 						
-						if (currentURL.indexOf("file://") != -1){
+						if (currentURL.indexOf("file:") != -1){
+							
 							var localStg_key = localStorage.getItem(key)
 
 							var re_url = url_addr.replace(localStorage.key(localStg_key),local_ip);
 							//var ch_url = re_url.substr(2)
-							var sum = https + re_url
+							var sum = https + re_url;
 							chrome.tabs.update({url: sum});
-							console.log(sum,re_url)
+							var ip = document.getElementById("ip");
+							var com_name = document.getElementById("com_name");
+							ip.innerHTML = local_ip;
+							com_name.innerHTML = localStorage.key(localStg_key)
+							console.log(sum,localStorage.key(localStg_key))
+						}
+						else {
+							alert(tabArray[0].status)
 						}
 					
 					}
 				}
 			})
 		}
-	})
+	});
+	
+
 }
 addLoadEvent(url_rep)
